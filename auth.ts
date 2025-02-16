d257d40978bc9ea45fb53094690873a98306f8ac
John Sison <132543192+jls777@users.noreply.github.com>
2025-02-16 05:56:38 -0500

    bcrypt ts edge

--- auth.ts
+++ auth.ts
@@ -2,8 +2,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
-// import { compareSync } from "bcrypt-ts-edge";
-import { compare } from "./lib/encrypt";
+import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
@@ -36,7 +35,7 @@ export const config = {
 
         //check if the user exist and if the password matches
         if (user && user.password) {
-          const isMatch = await compare(
+          const isMatch = await compareSync(
             credentials.password as string,
             user.password
           );
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -7,8 +7,7 @@ import {
 } from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
-// import { hashSync } from "bcrypt-ts-edge";
-import { hash } from "../encrypt";
+import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
@@ -53,8 +52,7 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
 
     const plainPassword = user.password;
 
-    // user.password = hashSync(user.password, 10);
-    user.password = await hash(user.password);
+    user.password = hashSync(user.password, 10);
 
     await prisma.user.create({
       data: {
