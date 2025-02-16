afbe1a7ca270cd8400e0e8e6c6e0593c6ed01891
John Sison <132543192+jls777@users.noreply.github.com>
2025-02-16 05:28:24 -0500

    protected path

--- auth.ts
+++ auth.ts
@@ -71,6 +71,7 @@ export const config = {
     async jwt({ token, user, trigger, session }: any) {
       //assign user fields to token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
         //if user has no name then use the email
         if (user.name === "NO_NAME") {
@@ -82,10 +83,57 @@ export const config = {
             data: { name: token.name },
           });
         }
+
+        if (trigger === "signIn" || trigger === "signUp") {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              //delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
       }
+
+      //handle session updates
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
+      }
+
       return token;
     },
     authorized({ request, auth }: any) {
+      // Array of regex patterns of paths we want to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      //get pathname from the req url object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       //check for session cart cookie
       if (!request.cookies.get("sessionCartId")) {
         //generate new session cart id cookie
